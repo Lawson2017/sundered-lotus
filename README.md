cc063bc598e6eee2d1344fe8d3db96ced1f69014
james lawson <jlawson079@gmail.com>
2020-05-26 15:21:06 -0700

    first com commit, built-in .css function and troubleshooting emmet plug-in

---
+++ README.md
@@ -0,0 +1,2 @@
+# Beginner-Advance selectors and built in .css funct in jQuery
+## Uncomment in source code to test different alterations in the DOM
---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+	//built in .css function to change properties
+	// $("p").css("background-color", "rgba(180,180,30,0.8)");
+	// $(".red-box").css("background-color", "rgba(180,180,30,0.8)");
+	// $("input[type='text']").css("background-color", "rgba(180,180,30,0.8)");
+	// $("h2,p,input").css("background-color", "rgba(180,180,30,0.8)");
+	//select first and last element only
+	// $("li:last").css("background-color", "rgba(180,180,30,0.8)");
+	// $("li:odd").css("background-color", "rgba(180,180,30,0.8)");
+
+	$("input:text").css("background-color", "rgba(180,180,30,0.8)")
+});
+
+//check W3 cheatsheat for all selectors
